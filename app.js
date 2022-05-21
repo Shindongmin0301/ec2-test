fb738550a99a537c2cff3db9cefdd72bcf7a9761
eastpeople0301@gmail.com <sindongmin@sindongmin-ui-MacBookAir.local>
2022-05-21 16:48:11 +0900

    commite before deploy3

--- app.js
+++ app.js
@@ -17,10 +17,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// 라우트 설정
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
