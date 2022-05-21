067921d039b9a77c8ada98a1c6ec5b415fce85de
eastpeople0301@gmail.com <sindongmin@sindongmin-ui-MacBookAir.local>
2022-05-21 17:42:20 +0900

    client- add route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>This is React!!</h1>
+      <Routes>
+        <Route path="/" element={<h1>Home page</h1>} />
+        <Route path="/user" element={<h1>User page</h1>} />
+        <Route path="/about" element={<h1>About pagr</h1>} />
+      </Routes>
     </div>
   );
 }
