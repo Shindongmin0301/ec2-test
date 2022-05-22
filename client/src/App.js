02d2cb9c076f00c92060b101a94c886345d6c3cd
eastpeople0301@gmail.com <sindongmin@sindongmin-ui-MacBookAir.local>
2022-05-22 13:09:37 +0900

    commit before deploy ec2

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Layout from './Components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/*" element={<h1>Layout</h1>}>
-          <Route path="/user" element={<h2>User page</h2>} />
-          <Route path="/about" element={<h1>About pagr</h1>} />
-        </Route>
-      </Routes>
+      <Layout />
+      <Home />
     </div>
   );
 }
